f6b5c88a3e2ab8afc1938bc2224b60fd7a6232a1
ryo-terada <ryo214099@gmail.com>
2022-05-29 21:51:22 +0000

    [Update]4-25

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -13,10 +13,24 @@ class BooksController < ApplicationController
   end
   
   def index
+    @user = current_user
     @books = Book.all
     @book = Book.new
-    @user = current_user
   end
+  
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      flash[:notice]="You have creatad book successfully."
+      redirect_to book_path(@book)
+    else
+      @user = current_user
+      @books = Book.all
+      render :index
+    end
+  end
+
 
   def show
     @book = Book.find(params[:id])
@@ -57,6 +71,4 @@ class BooksController < ApplicationController
   def book_params
     params.require(:book).permit(:title, :body)
   end
-  
-  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   
-  resources :books, only: [:new, :create, :index, :show, :destroy, :edit]
+  resources :books
   resources :users, only: [:show, :edit, :update, :index]
   
   root to: 'homes#top'
